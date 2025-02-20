d433e1e100c2a32309be6556f063a05fedb46ce8
senoldogann <s.dogann@yandex.com>
2025-02-21 01:44:00 +0200

    2

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
 }
 
-// PrismaClient için global tip tanımı
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
-
-const prisma = globalThis.prisma ?? prismaClientSingleton()
+const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = prisma
+  globalForPrisma.prisma = prisma
 }
 
 export default prisma 
