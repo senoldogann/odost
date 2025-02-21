26a18f82ccc9f404ecdaa1e0f601d0ecbf8905fe
senoldogann <s.dogann@yandex.com>
2025-02-21 02:32:42 +0200

    update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,14 +4,20 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-const prisma = globalThis.prisma ?? new PrismaClient({
-  log: ['query'],
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: ['query'],
+    datasources: {
+      db: {
+        url: process.env.NODE_ENV === 'production' 
+          ? process.env.DATABASE_URL 
+          : process.env.DIRECT_URL
+      }
     }
-  }
-})
+  })
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
 
