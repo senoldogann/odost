3310dd02a9365978b8728984fee13260eba274b1
senoldogann <s.dogann@yandex.com>
2025-02-21 02:16:23 +0200

    update

--- next.config.js
+++ next.config.js
@@ -34,7 +34,9 @@ const nextConfig = {
     ignoreDuringBuilds: true
   },
   experimental: {
-    serverActions: true
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'odostlounas.vercel.app']
+    }
   },
   poweredByHeader: false,
   reactStrictMode: true,
